a0d4d4b809dbfdd388a113fb441d07b1cf32366b
HRegniez <henriregniez124@gmail.com>
2024-06-11 19:48:34 +0200

    update

--- recipe-page/app/Nutrition.jsx
+++ recipe-page/app/Nutrition.jsx
@@ -38,18 +38,3 @@ function Nutrition() {
 
 export default Nutrition
 
-// Nutrition
-
-// The table below shows nutritional values per serving without the additional fillings.
-
-// Calories
-// 277kcal
-
-// Carbs
-// 0g
-
-// Protein
-// 20g
-
-// Fat
-// 22g
